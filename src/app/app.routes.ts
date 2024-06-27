56e9317c3ad81ed8bd5e62cb26cd53ecaf36414f
nyx <nyx.law.official@gmail.com>
2024-06-27 15:44:56 +0800

    feat(shopping-cart-page): 新增購物車元件

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { productResolver } from './resolver/product.resolver';
+import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component'
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products'},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     component: ProductDetailPageComponent,
     resolve: { product: productResolver }
   },
+  { path: 'shopping-cart', component: ShoppingCartPageComponent}
 ];
